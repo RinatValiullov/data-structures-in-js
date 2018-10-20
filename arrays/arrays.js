65ac53d8fb24254078fffcf51572c8d181424c43
RinatValiullov <4rontender@gmail.com>
2018-10-20 15:59:19 +0500

    added sorting and other functions

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -4,6 +4,32 @@ const positiveNumbers = [1, 2, 3];
 
 const negativeNumbers = [-1, -2, -3];
 
-const numbers = negativeNumbers.concat(zero, positiveNumbers);
+const numbers = positiveNumbers.concat(zero, negativeNumbers);
 
-console.log(numbers);
+// console.log(numbers);
+
+const numbers1 = [9, 5, 6, 4, 7, 8, 3];
+
+const isEven = x => {
+    return x % 2 === 0 ? true : false;
+};
+
+const withEvery = numbers1.every(isEven);
+
+const withForEach = numbers1.forEach(x => console.log(x % 2 === 0));
+
+const withFilter = numbers1.filter(isEven);
+
+// console.log(withEvery);
+// console.log(withForEach);
+// console.log(withFilter);
+
+const compare = (a, b) => {
+    return a === b ? 0 : a < b ? -1 : 1;
+};
+
+const compareNumbers = (a, b) => a - b;
+
+console.log(numbers1.sort(compareNumbers));
+
+console.log(numbers1.sort(compare));
