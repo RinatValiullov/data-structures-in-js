f0f4130b441307645dff6c0dcdf4e93f40c5d243
RinatValiullov <4rontender@gmail.com>
2018-10-20 13:30:18 +0500

    added arrays.js file

---
+++ arrays/arrays.js
@@ -0,0 +1,9 @@
+const zero = 0;
+
+const positiveNumbers = [1, 2, 3];
+
+const negativeNumbers = [-1, -2, -3];
+
+const numbers = negativeNumbers.concat(zero, positiveNumbers);
+
+console.log(numbers);
